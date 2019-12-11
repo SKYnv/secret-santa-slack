5e18acd6bac799cb9c6f0749528da738a9c317bd
Aleksey <kvazarv@ya.ru>
2019-12-11 12:26:31 +0700

    initial

---
+++ main.py
@@ -0,0 +1,21 @@
+from aiohttp import web
+
+async def handle(request):
+    return web.Response(text="Hello!")
+
+async def unknown(request):
+    url = request.match_info.get('url')
+    text = f'Unknown enpoint "{url}".'
+    return web.Response(text=text)
+
+async def incomming_message(request):
+    response = await request.json()
+    return web.json_response(data=response)
+
+app = web.Application()
+app.add_routes([web.get('/', handle),
+                web.get('/msg', incomming_message),
+                web.get('/{url}', unknown),])
+
+if __name__ == '__main__':
+    web.run_app(app)
