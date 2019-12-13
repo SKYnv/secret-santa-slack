20655372a33d68c1ca3a4d29be9955e204911737
Aleksey <kvazarv@ya.ru>
2019-12-14 01:42:35 +0700

    conf and reply too message

--- conf/settings.py
+++ conf/settings.py
@@ -2,3 +2,4 @@ import os
 
 PORT = os.getenv("PORT", 80)
 DATABASE_URL = os.getenv('DATABASE_URL')
+SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from aiohttp import web
 from json.decoder import JSONDecodeError
+from aioslacker import Slacker
+from conf.settings import SLACK_BOT_TOKEN
 
 async def handle(request):
     return web.Response(text="Hello!")
@@ -12,16 +14,25 @@ async def unknown(request):
 async def incomming_message(request):
     try:
         response = await request.json()
-    except JSONDecodeError:
-        print(request)
-        response = {}
+        print(response)
+    except Exception:
+        return web.Response(text=request.headers)
 
     if request.method == 'POST':
-        print(response)
-        challenge = response.get('challenge')
-        if challenge:
-            return web.json_response(data={"challenge": challenge})
+        if 'challenge' in response:
+            return web.json_response(data={"challenge": response.get('challenge')})
+
+        if response["event"]["type"] == "message":
+            await reply_to_message(response["event"])
+
+        return web.Response()
+
     return web.json_response(data=response)
+
+async def reply_to_message(event):
+    text = (f"Ой <@{event['user']}>, я еще такой глупый! ")
+
+    await slack_client.chat.post_message(event["channel"], text=text)
 
 santa_app = web.Application()
 santa_app.add_routes([web.get('/', handle),
@@ -30,4 +41,5 @@ santa_app.add_routes([web.get('/', handle),
                 web.get('/{url}', unknown),])
 
 if __name__ == '__main__':
+    slack_client = Slacker(SLACK_BOT_TOKEN)
     web.run_app(santa_app, port=80)
